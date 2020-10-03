93da22eefa2efcb0abbd96ea71fb4070cace3b1d
froggagul <froggagul@gmail.com>
2020-10-04 01:51:53 +0900

    cnn-inception code added

---
+++ cnn-inception.py
@@ -0,0 +1,132 @@
+from __future__ import print_function
+import argparse
+import torch
+from torch import nn, optim, cuda
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import time
+device = 'cuda' if cuda.is_available() else 'cpu'
+print(f'Training MNIST Model on {device}\n{"=" * 44}')
+
+batch_size = 64
+
+train_dataset = datasets.MNIST(root='./data',
+                              train=True,
+                              transform=transforms.ToTensor(),
+                              download=True)
+test_dataset = datasets.MNIST(root='./data/',
+                              train=False,
+                              transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+class InceptionA(nn.Module):
+  def __init__(self, in_channels):
+    super(InceptionA, self).__init__()
+    self.branch1x1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+    self.branch_pool_1 = nn.AvgPool2d(kernel_size=3, stride=1, padding=1)
+    self.branch_pool_2 = nn.Conv2d(in_channels, 24, kernel_size=1)
+
+    self.branch5x5_1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+    self.branch5x5_2 = nn.Conv2d(16, 24, kernel_size=5, padding=2)
+
+    self.branch3x3dbl_1 = nn.Conv2d(in_channels, 16, kernel_size=1)
+    self.branch3x3dbl_2 = nn.Conv2d(16, 24, kernel_size=3, padding=1)
+    self.branch3x3dbl_3 = nn.Conv2d(24, 24, kernel_size=3, padding=1)
+
+  def forward(self, x):
+    branch1x1 = self.branch1x1(x)
+    
+    branch_pool = self.branch_pool_1(x)
+    branch_pool = self.branch_pool_2(branch_pool)
+
+    branch5x5 = self.branch5x5_1(x)
+    branch5x5 = self.branch5x5_2(branch5x5)
+
+    branch3x3dbl = self.branch3x3dbl_1(x)
+    branch3x3dbl = self.branch3x3dbl_2(branch3x3dbl)
+    branch3x3dbl = self.branch3x3dbl_3(branch3x3dbl)
+
+    outputs = [branch1x1, branch5x5, branch3x3dbl, branch_pool]
+    return torch.cat(outputs, 1)
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+    # 24 + 24 + 24 + 16 = 88
+    self.conv2 = nn.Conv2d(88, 20, kernel_size=5)
+
+    self.incept1 = InceptionA(in_channels=10)
+    self.incept2 = InceptionA(in_channels=20)
+
+    self.mp = nn.MaxPool2d(2)
+    self.fc = nn.Linear(1408, 10)
+
+  def forward(self, x):
+    in_size = x.size(0)
+    x = F.relu(self.mp(self.conv1(x)))
+    x = self.incept1(x)
+    x = F.relu(self.mp(self.conv2(x)))
+    x = self.incept2(x)
+    x = x.view(in_size, -1)  # flatten the tensor
+    x = self.fc(x)
+    return F.log_softmax(x)
+
+model = Net()
+model.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad() # 기울기 초기화
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()  # back propagation, 역전파
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        # sum up batch loss
+        test_loss += criterion(output, target).item()
+        # get the index of the max
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print(f'===========================\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} '
+          f'({100. * correct / len(test_loader.dataset):.0f}%)')
+
+
+if __name__ == '__main__':
+    since = time.time()
+    for epoch in range(1, 10):
+        epoch_start = time.time()
+        train(epoch)
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Training time: {m:.0f}m {s:.0f}s')
+        test()
+        m, s = divmod(time.time() - epoch_start, 60)
+        print(f'Testing time: {m:.0f}m {s:.0f}s')
+
+    m, s = divmod(time.time() - since, 60)
+    print(f'Total Time: {m:.0f}m {s:.0f}s\nModel was trained on {device}!')
